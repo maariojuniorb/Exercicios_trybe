a331513d9158325da127d5473371428a6cddf7ab
Mario Bispo Junior <maariojuniorb@gmail.com>
2022-06-01 12:54:28 -0300

    exercicios dia 6.1

---
+++ fundamentos/HTML-CSS-FORMS-FLEXBOX-RESPONSIVO/6.1/exercicio-1/script2.js
@@ -0,0 +1,10 @@
+let buttonEnviar = document.querySelector('#btn-enviar')
+let buttonClear = document.querySelector('#btn-clear')
+
+buttonEnviar.addEventListener('click', (event) => {
+  event.preventDefault();
+})
+
+// buttonClear.addEventListener('click', (event) => {
+//   event.preventDefault();
+// })
